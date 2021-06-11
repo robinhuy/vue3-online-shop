ad04fc87b3e325ac698ac83d6eab3ad1be4c71ae
Robin Huy <12640832+robinhuy@users.noreply.github.com>
2021-06-11 22:32:55 +0700

    move css & fonts to public folder

--- src/main.js
+++ src/main.js
@@ -3,12 +3,6 @@ import App from "./App.vue";
 import router from "@/router/index";
 import store from "@/store/index";
 
-import "@/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css";
-import "@/assets/css/bootstrap.min.css";
-import "@/assets/css/hamburgers.min.css";
-import "@/assets/css/main.css";
-import "@/assets/css/util.css";
-
 const app = createApp(App);
 app.use(router);
 app.use(store);
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production" ? "/vue3-online-shop/" : "/",
+  configureWebpack: {
+    performance: {
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+    },
+  },
 };
