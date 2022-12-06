c94e8257ec15ca46b50861940fdfe389352ea0ff
HuyDQ <huydq@ltsgroup.tech>
2022-12-06 09:59:22 +0700

    update depedencies

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const isProductionEnv = process.env.NODE_ENV === "production";
 
 export const API_DOMAIN = isProductionEnv
   ? "https://online-shop-apis.herokuapp.com"
-  : "http://localhost:3000";
+  : "http://localhost:8000";
 
 export const BASE_URL = isProductionEnv ? "/vue3-online-shop/" : "/";
 
